8cfd1419c67064cb42e8ef0c4b42b89bf74edc4d
Shantel <Shantelgray24@gmail.com>
2025-05-06 14:39:25 -0400

    Adding conditionals to the control flow lab

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function lateFee() {
-  // Write your code here!
+function lateFee(numberOfDays) {
+  if (numberOfDays <= 0)  {
+    return "No fine!"
+  } else {
+    if (numberOfDays <= 7) {
+      return 'Your fine is $5.'
+    } else if (numberOfDays <= 10) {
+      return "Your fine is $20."
+    } else {
+       if (numberOfDays >= 31) {
+        return "Membership suspended."
+       }
+    }
+  }
 }
 
 function branchHours() {
